09c4e18acda5123bea79ddb495a1b052e397ab6f
naumshubaev <naum.shubaev@gmail.com>
2022-05-17 10:28:33 +0300

    initial commit tmp

---
+++ src/test/java/ru/netology/CardDelivery.java
@@ -0,0 +1,77 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class CardDelivery {
+
+    private Faker faker;
+
+    @BeforeEach
+    void setUpAll() {
+        faker = new Faker(new Locale("ru"));
+    }
+
+    String deliveryDate() {
+        // deliveryDate == today+daysAdded
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+// RANDOM will not work for the first click actually
+        int daysAdded = 3;
+                // for Random: ThreadLocalRandom.current().nextInt(3, 20);
+
+        return today.plusDays(daysAdded).format(dtf);
+    }
+
+    @Test
+    public void shouldOrderCardDelivery() {
+
+        String name = faker.name().fullName();
+        String phone = faker.phoneNumber().subscriberNumber(10); //test: should be 10
+        // another option for temporary workaround - the region code problem still exists
+ //       String phoneF = faker.numerify("##########");
+// move faker to utility class
+            // just for visualisation
+        System.out.println(name);
+        System.out.println(phone);
+        System.out.println(deliveryDate());
+//        System.out.println(phoneF);
+
+        Configuration.headless = false;
+        Configuration.holdBrowserOpen = false;
+        // fastSetValue - I have no idea what it is)))
+//        Configuration.fastSetValue = true;
+        open("http://localhost:9999");
+
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+//        $("[data-test-id='date'] input").doubleClick().sendKeys(deliveryDate());
+        $("[data-test-id='name'] input").setValue("Василий Пупкин");
+        $("[data-test-id='phone'] input").val("+78121234567");
+        $("[data-test-id='agreement']>span").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $("[class='notification__content']")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + deliveryDate())
+                        , Duration.ofSeconds(15));
+
+// при повторном нажатии появляется всплывающее окно с корректным текстом, но Селенид его не находит
+        $$("button").find(exactText("Запланировать")).click();
+        $x("[//*[@id=\"root\"]/div/div[2]/div[3]/text()]")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?")
+                        , Duration.ofSeconds(15));
+
+    }
+}
